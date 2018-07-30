e0d9180b246fa8dbeafa133d3e464538b0a889b5
djiadjia <jia.wu@fdt.ai>
2018-07-30 16:13:41 +0800

    modified Detector.py and Classifier.py

--- cvtron-serve/endpoint/Classifier.py
+++ cvtron-serve/endpoint/Classifier.py
@@ -11,6 +11,7 @@ from cvtron.modeling.classifier import api
 from cvtron.data_zoo.compress_util import ArchiveFile
 from cvtron.utils.reporter import print_prob
 from cvtron.trainers.classifier.slim_classifier_trainer import SlimClassifierTrainer
+from cvtron.modeling.classifier.slim_classifier import SlimClassifier
 
 from .config import BASE_FILE_PATH
 from .config import STATIC_FILE_PATH
@@ -26,20 +27,25 @@ class Classifier(object):
             self.folder_name = 'img_' + str(uuid.uuid4()).split('-')[0]
         else:
             self.folder_name = folder_name
-        # self.classifier = api.get_classifier()
+        self.ready_to_infer = False
 
-    def process_result(self, result):
-        json_result = []
-        for each in result:
-            result_dic = {'type': each[0], 'prob': str(each[1])}
-            json_result.append(result_dic)
-        return json_result
+    def _init_inference(self, model_id, test_pic_dir):
+        self.classifier = SlimClassifier()
+        base_path = model_id
+        base_path = os.path.join(self.BASE_FILE_PATH, base_path)
+        if not os.path.exists(base_path):
+            raise cherrypy.HTTPError(404)
+        model_name = model_id
+        model_path = base_path
+        self.classifier.init(model_name, base_path)
+        self.ready_to_infer = True
 
+    @cherrypy.tools.json_out()
     @cherrypy.config(**{'tools.cors.on': True})
     @cherrypy.expose
     def classify(self, ufile):
-        if not self.classifier:
-            self.classifier = api.get_classifier()
+        model_name = cherrypy.request.params.get('model_name')
+        # Handler for model: model_name
         upload_path = os.path.join(self.BASE_FILE_PATH, self.folder_name)
         if not os.path.exists(upload_path):
             os.makedirs(upload_path)
@@ -52,9 +58,17 @@ class Classifier(object):
                     break
                 out.write(data)
                 size += len(data)
-        topn = print_prob(self.classifier.classify(upload_file), 5)
-        out = {'result': self.process_result(topn)}
-        return json.dumps(out)
+        # if not self.ready_to_infer:
+        #     self._init_inference(model_name, upload_path)
+        base_path = os.path.join(self.BASE_FILE_PATH, 'img_d_1234567890')
+        model_name = 'inception_v1'
+        self.classifier = SlimClassifier()
+        results =  self.classifier.classify(upload_file, model_name, base_path)
+        print(results)
+        response = {
+            'results': results
+        }
+        return results        
 
     @cherrypy.config(**{'tools.cors.on': True})
     @cherrypy.expose
@@ -72,17 +86,24 @@ class Classifier(object):
                     break
                 out.write(data)
                 size += len(data)
+        upload_path = os.path.join(self.BASE_FILE_PATH, 'img_d_1234567890') 
+        upload_file = os.path.join(upload_path, 'flowers.zip')
         uncompress_path = upload_path
         af = ArchiveFile(upload_file)
         af.unzip(uncompress_path, deleteOrigin=False)
-        configs = {
-            'fine_tune_ckpt': os.path.join(upload_path, 'pretrained/model.ckpt'),
-            'data_dir': upload_path,
-            'weblog_dir': os.path.join(STATIC_FILE_PATH, fid),
-            'train_dir': upload_path,
-        }
-        configs['pre-trained_model'] = 'inception_v1'
-        self.trainer = SlimClassifierTrainer(configs, upload_path)
+        modelFile = '/home/ubuntu/.cvtron/model_zoo/inception_v1.zip'
+        modelZip = ArchiveFile(modelFile)
+        modelZip.unzip(upload_path, deleteOrigin=False)
+
+        # configs = {
+        #     'fine_tune_ckpt': os.path.join(upload_path, 'model.ckpt'),
+        #     'data_dir': upload_path,
+        #     'weblog_dir': os.path.join(STATIC_FILE_PATH, fid),
+        #     'train_dir': upload_path,
+        # }
+        # configs['pre-trained_model'] = 'inception_v1'
+        # upload_path = os.path.join(self.BASE_FILE_PATH, 'img_d_1234567890')
+        self.trainer = SlimClassifierTrainer({}, upload_path)
         self.trainer.parse_dataset(os.path.join(upload_path, 'annotations.json'))
         result = {
             'result': 'success',
@@ -96,7 +117,7 @@ class Classifier(object):
         config = {
           'batch_size': 24,          
           'optimizer':'rmsprop',
-          'log_every_n_steps': 1,
+          'log_every_n_steps': 100,
           'learning_rate': 0.0001
         }
         return json.dumps(config)
@@ -107,17 +128,23 @@ class Classifier(object):
         cl = cherrypy.request.headers['Content-Length']
         rawbody = cherrypy.request.body.read(int(cl))
         config = json.loads(rawbody.decode('utf-8'))
+        print(config)
         try:
             request_id = config['file']
             request_folder_name = 'img_d_' + request_id
-            train_path = os.path.join(self.BASE_FILE_PATH, request_folder_name)
+            # train_path = os.path.join(self.BASE_FILE_PATH, request_folder_name)
+            train_path = os.path.join(self.BASE_FILE_PATH, 'img_d_1234567890')
             configs = {
-                'fine_tune_ckpt': os.path.join(train_path, 'pretrained/model.ckpt'),
+                'fine_tune_ckpt': os.path.join(train_path, 'model.ckpt'),
                 'data_dir': train_path,
                 'weblog_dir': os.path.join(STATIC_FILE_PATH, request_id),
                 'train_dir': train_path,
             }
+            configs['pre-trained_model'] = 'inception_v1'
+            configs.update(config['config'])
+
             self.trainer = SlimClassifierTrainer(configs, train_path)
+            self.trainer.set_dataset_info(os.path.join(train_path, 'annotations.json'))
             _thread.start_new_thread(self.trainer.start, ())
             result = {'config': config, 'log_file_name': request_id + '/log.json', 'taskId': request_id}
             return json.dumps(result)
--- cvtron-serve/endpoint/Detector.py
+++ cvtron-serve/endpoint/Detector.py
@@ -17,11 +17,15 @@ from cvtron.trainers.detector.object_detection_trainer import ObjectDetectionTra
 from .cors import cors
 from .config import BASE_FILE_PATH
 from .config import STATIC_FILE_PATH
+sys.path.append('..')
+from utils import inform
 
 cherrypy.tools.cors = cherrypy._cptools.HandlerTool(cors)
 
 class Detector(object):
     def __init__(self, folder_name=None):
+        # os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+        # os.environ["CUDA_VISIBLE_DEVICES"] = str(1) 
         self.BASE_FILE_PATH = BASE_FILE_PATH
         self.id = str(uuid.uuid4()).split('-')[0]
         self.ready_to_infer = False
@@ -30,14 +34,19 @@ class Detector(object):
         else:   
             self.folder_name = folder_name
 
-    def _init_inference(self, model_id):
+    def _init_inference(self, model_id, test_pic_dir):
         self.sod = SlimObjectDetector()
         base_path = model_id
         base_path = os.path.join(self.BASE_FILE_PATH, base_path)
         if not os.path.exists(base_path):
             raise cherrypy.HTTPError(404)
         label_map_path = os.path.join(base_path, 'label_map.pbtxt')
-        ckpt_path = os.path.join(base_path, 'frozen_inference_graph.pb')
+        # ckpt_path = os.path.join(base_path, 'frozen_inference_graph.pb')
+        exported_dir = os.path.join(test_pic_dir, 'exported')
+        ckpt_path = os.path.join(exported_dir, 'frozen_inference_graph.pb')
+        if os.path.exists(os.path.abspath(exported_dir)):
+            shutil.rmtree(os.path.abspath(exported_dir))
+        self.sod.export_latest_ckpt(base_path, exported_dir)
         self.sod.set_label_map(label_map_path)
         self.sod.init(ckpt_path)
         self.ready_to_infer = True
@@ -53,8 +62,9 @@ class Detector(object):
     @cherrypy.expose
     def detect(self, ufile, model_name):
         model_name = cherrypy.request.params.get('model_name')
+        # model_name = 'da67f84dc38f44d9943a9cdc56cab7f5'
         # Handler for model: model_name
-        detector = api.get_detector()
+        # detector = api.get_detector()
         upload_path = os.path.join(self.BASE_FILE_PATH, self.folder_name)
         if not os.path.exists(upload_path):
             os.makedirs(upload_path)
@@ -68,7 +78,8 @@ class Detector(object):
                 out.write(data)
                 size += len(data)
         if not self.ready_to_infer:
-            self._init_inference(model_name)
+            self._init_inference(model_name, upload_path)
+            # self.ready_to_infer = False
         results =  self.sod.detect(upload_file)
         response = {
             'results': results
@@ -77,7 +88,8 @@ class Detector(object):
 
     @cherrypy.config(**{'tools.cors.on': True})
     @cherrypy.expose
-    def upload(self, ufile):
+    def upload(self, ufile, modelId):
+        print('upload')
         fid = uuid.uuid4().hex
         upload_path = os.path.join(self.BASE_FILE_PATH, fid)
         if not os.path.exists(upload_path):
@@ -99,22 +111,25 @@ class Detector(object):
         af = ArchiveFile(upload_file)
         ### Delete Origin File to save disk space
         af.unzip(uncompress_path, deleteOrigin=True)
+        print(modelId)
         modelFile = '/home/ubuntu/.cvtron/model_zoo/ssd_inception_v2_coco_11_06_2017.zip'
+        # modelFile = '/home/wujia/.cvtron/model_zoo/ssd_inception_v2_coco_11_06_2017.zip'
         modelZip = ArchiveFile(modelFile)
         modelZip.unzip(upload_path, deleteOrigin=False)
 
-        train_config = {
-            'pipeline_config_file': os.path.join(upload_path, 'pipeline.config'),
-            'weblog_dir': os.path.join(STATIC_FILE_PATH, fid),
-            'log_every_n_steps':1,
-            'train_dir': upload_path,
-            'fine_tune_ckpt': os.path.join(upload_path, 'pretrained/model.ckpt'),
-            'data_dir': upload_path
-        }
-        train_config['pre-trained_model'] = 'ssd_mobilenet_v1'
-        self.trainer = ObjectDetectionTrainer(train_config, upload_path)
+        # train_config = {
+        #     'pipeline_config_file': os.path.join(upload_path, 'pipeline.config'),
+        #     'weblog_dir': os.path.join(STATIC_FILE_PATH, fid),
+        #     'log_every_n_steps':1,
+        #     'train_dir': upload_path,
+        #     'fine_tune_ckpt': os.path.join(upload_path, 'pretrained/model.ckpt'),
+        #     'data_dir': upload_path
+        # }
+        # train_config['pre-trained_model'] = 'ssd_inception_v2'
+        self.trainer = ObjectDetectionTrainer({}, upload_path)
         self.trainer.parse_dataset(os.path.join(upload_path, 'annotations.json'))
         result = {'result': 'success', 'file_id': fid}
+        print(result)
         return json.dumps(result)
 
     @cherrypy.config(**{'tools.cors.on': True})
@@ -124,7 +139,7 @@ class Detector(object):
             'batch_size':24,
             'learning_rate':0.001,
             'num_steps':200000,
-            'log_every_n_steps':1
+            'log_every_n_steps':100
         }
         return json.dumps(config)
 
@@ -134,6 +149,7 @@ class Detector(object):
         cl = cherrypy.request.headers['Content-Length']
         rawbody = cherrypy.request.body.read(int(cl))
         config = json.loads(rawbody.decode('utf-8'))
+        print(config)
         try:
             override_config = config['config']
             request_id = config['file']
@@ -142,7 +158,7 @@ class Detector(object):
             train_config = {
                 'pipeline_config_file': os.path.join(train_path, 'pipeline.config'),
                 'weblog_dir': os.path.join(STATIC_FILE_PATH, request_id),
-                'log_every_n_steps':1,
+                'log_every_n_steps':config['config']['log_every_n_steps'],
                 'train_dir': train_path,
                 'fine_tune_ckpt': os.path.join(train_path, 'model.ckpt'),
                 'data_dir': train_path
@@ -151,7 +167,8 @@ class Detector(object):
             self.trainer.set_annotation(os.path.join(train_path, 'annotations.json'))
             self.trainer.override_pipeline_config(override_config, os.path.join(train_path, 'pipeline.config'))
             # pid = os.fork()
-            _thread.start_new_thread(self.trainer.start, ())
+            emailAddr = 'jia.wu@szu.edu.cn'
+            _thread.start_new_thread(self.trainer.start, (inform, (request_id, emailAddr)))
             result = {'config': config, 'log_file_name': request_id + '/log.json', 'taskId': request_id}
             return json.dumps(result)
         except Exception:
@@ -162,6 +179,7 @@ class Detector(object):
     def get_model(self, model_id):
         always_refresh = True
         model_id = cherrypy.request.params.get('model_id')
+        print(model_id)
         request_folder_name = model_id
         train_path = os.path.join(self.BASE_FILE_PATH, request_folder_name)
         if not os.path.exists(train_path):
@@ -175,13 +193,14 @@ class Detector(object):
             logger.info('model exists,skipping')
             result = {
                 'code': '200',
-                'url': 'http://118.89.28.34:9090/static/' + model_id + '/' + model_id + '.zip'
+                'url': 'http://134.175.1.246:80/static/' + model_id + '/' + model_id + '.zip'
             }
             return json.dumps(result)
         else:
             taf.zip(compressedFile)
             result = {
                 'code': '200',
-                'url': 'http://118.89.28.34:9090/static/' + model_id + '/' + model_id + '.zip'
+                'url': 'http://134.175.1.246:80/static/' + model_id + '/' + model_id + '.zip'
             }
             return json.dumps(result)
+
