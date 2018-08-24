dd7e01d7de3513a9510d0cadeee5d6125afbe158
ubuntu <ubuntu@localhost.localdomain>
2018-08-24 10:28:04 +0800

    add model path, model map, notification callback

--- cvtron-serve/endpoint/Classifier.py
+++ cvtron-serve/endpoint/Classifier.py
@@ -13,12 +13,33 @@ from cvtron.utils.reporter import print_prob
 from cvtron.trainers.classifier.slim_classifier_trainer import SlimClassifierTrainer
 from cvtron.modeling.classifier.slim_classifier import SlimClassifier
 
+from .config import MODEL_PATH
 from .config import BASE_FILE_PATH
 from .config import STATIC_FILE_PATH
 from .cors import cors
+sys.path.append('..')
+from utils import inform
 
 cherrypy.tools.cors = cherrypy._cptools.HandlerTool(cors)
 
+_MODEL_MAP = {
+    'alexnet': '',
+    'inception_v1': 'inception_v1_2016_08_28.zip',
+    'inception_v2': 'inception_v2_2016_08_28.zip',
+    'inception_v3': 'inception_v3_2016_08_28.zip',
+    'inception_v4': 'inception_v4_2016_08_28.zip',
+    'mobilenet_v1': 'mobilenet_v1_1.0_224.zip',
+    'nasnet-a_mobile': 'nasnet-a_mobile_04_10_2017.zip',
+    'nasnet-a_large': 'nasnet-a_large_04_10_2017.zip',
+    'resnet_v1_50': 'resnet_v1_50_2016_08_28.zip',
+    'resnet_v1_101': 'resnet_v1_101_2016_08_28.zip',
+    'resnet_v1_152': 'resnet_v1_152_2016_08_28.zip',
+    'resnet_v2_50': 'resnet_v2_50_2017_04_14.zip',
+    'resnet_v2_101': 'resnet_v2_101_2017_04_14.zip',
+    'resnet_v2_152': 'resnet_v2_152_2017_04_14.zip',  
+    'vgg_16': 'vgg_16_2016_08_28.zip',
+    'vgg_19': 'vgg_19_2016_08_28.zip',  
+}
 
 class Classifier(object):
     def __init__(self, folder_name=None):
@@ -44,7 +65,7 @@ class Classifier(object):
     @cherrypy.config(**{'tools.cors.on': True})
     @cherrypy.expose
     def classify(self, ufile):
-        model_name = cherrypy.request.params.get('model_name')
+        # model_id = cherrypy.request.params.get('model_name')
         # Handler for model: model_name
         upload_path = os.path.join(self.BASE_FILE_PATH, self.folder_name)
         if not os.path.exists(upload_path):
@@ -61,7 +82,8 @@ class Classifier(object):
         # if not self.ready_to_infer:
         #     self._init_inference(model_name, upload_path)
         base_path = os.path.join(self.BASE_FILE_PATH, 'img_d_1234567890')
-        model_name = 'inception_v1'
+        # model_name = 'inception_v1'
+        model_name = self.modelId
         self.classifier = SlimClassifier()
         results =  self.classifier.classify(upload_file, model_name, base_path)
         print(results)
@@ -72,7 +94,8 @@ class Classifier(object):
 
     @cherrypy.config(**{'tools.cors.on': True})
     @cherrypy.expose
-    def upload(self, ufile):
+    def upload(self, ufile, modelId):
+        self.modelId = modelId
         fid = uuid.uuid4().hex
         upload_path = os.path.join(self.BASE_FILE_PATH, fid)
         if not os.path.exists(upload_path):
@@ -91,18 +114,11 @@ class Classifier(object):
         uncompress_path = upload_path
         af = ArchiveFile(upload_file)
         af.unzip(uncompress_path, deleteOrigin=False)
-        modelFile = '/home/ubuntu/.cvtron/model_zoo/inception_v1.zip'
+        modelFile = os.path.join(os.path.join(MODEL_PATH, '.cvtron/model_zoo'), _MODEL_MAP[self.modelId])
+        # modelFile = '/home/ubuntu/.cvtron/model_zoo/inception_v1.zip'
         modelZip = ArchiveFile(modelFile)
         modelZip.unzip(upload_path, deleteOrigin=False)
 
-        # configs = {
-        #     'fine_tune_ckpt': os.path.join(upload_path, 'model.ckpt'),
-        #     'data_dir': upload_path,
-        #     'weblog_dir': os.path.join(STATIC_FILE_PATH, fid),
-        #     'train_dir': upload_path,
-        # }
-        # configs['pre-trained_model'] = 'inception_v1'
-        # upload_path = os.path.join(self.BASE_FILE_PATH, 'img_d_1234567890')
         self.trainer = SlimClassifierTrainer({}, upload_path)
         self.trainer.parse_dataset(os.path.join(upload_path, 'annotations.json'))
         result = {
@@ -142,11 +158,40 @@ class Classifier(object):
             }
             configs['pre-trained_model'] = 'inception_v1'
             configs.update(config['config'])
-
             self.trainer = SlimClassifierTrainer(configs, train_path)
             self.trainer.set_dataset_info(os.path.join(train_path, 'annotations.json'))
-            _thread.start_new_thread(self.trainer.start, ())
+            emailAddr = 'jia.wu@szu.edu.cn'
+            _thread.start_new_thread(self.trainer.start, (inform, (request_id, emailAddr)))
             result = {'config': config, 'log_file_name': request_id + '/log.json', 'taskId': request_id}
             return json.dumps(result)
         except Exception:
             traceback.print_exc(file=sys.stdout)
+
+    @cherrypy.config(**{'tools.cors.on': True})
+    @cherrypy.expose
+    def get_model(self, model_id):
+        always_refresh = True
+        model_id = cherrypy.request.params.get('model_id')
+        request_folder_name = model_id
+        train_path = os.path.join(self.BASE_FILE_PATH, request_folder_name)
+        if not os.path.exists(train_path):
+            raise cherrypy.HTTPError(404)
+        taf = ToArchiveFolder(train_path)
+        compressedFile = os.path.join(STATIC_FILE_PATH, model_id)
+        if not os.path.exists(compressedFile):
+            os.makedirs(compressedFile)
+        compressedFile = os.path.join(compressedFile, model_id + '.zip')
+        if os.path.exists(compressedFile) and not always_refresh:
+            logger.info('model exists,skipping')
+            result = {
+                'code': '200',
+                'url': 'http://134.175.1.246:80/static/' + model_id + '/' + model_id + '.zip'
+            }
+            return json.dumps(result)
+        else:
+            taf.zip(compressedFile)
+            result = {
+                'code': '200',
+                'url': 'http://134.175.1.246:80/static/' + model_id + '/' + model_id + '.zip'
+            }
+            return json.dumps(result)            
